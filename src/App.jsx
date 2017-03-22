8e63b332aeb314711c84776028ec7c314fecab29
Damini Sinha <Damini Sinha>
2017-03-22 22:02:45 +0530

    Login with bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import LoginButton from './LoginButton.jsx';
+
 export default class App extends React.Component{
 
   render(){
--- src/LoginButton.jsx
+++ src/LoginButton.jsx
@@ -2,19 +2,30 @@ import React from 'react';
 import LoginCredentials from './LoginCredentials.jsx';
 
 export  default class LoginButton extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {showComponent:false};
-      this.handleEvent = this.handleEvent.bind(this);
-    }
-    handleEvent(){
-      this.setState({showComponent:true});
-  }
+
   render(){
     return(
       <div>
-      <input type='button' value="Login" onClick={this.handleEvent}/>
-      {this.state.showComponent?<LoginCredentials />:null}
+      <input type='button' className="btn btn-success btn-lg" value="Login" data-toggle="modal" data-target="#myModal"/>
+      <div className="modal fade" id="myModal" role="dialog">
+        <div className="modal-dialog modal-m">
+          <div className="modal-content">
+            <div className="modal-header text-center">
+              <h4 className="modal-title">Sign in to SmartAudit</h4>
+            </div>
+            <div className="modal-body text-center" >
+            <label>Enter your email and password </label><br/>
+            <input type="text" className="form-control input-lg"  placeholder="Email"/>
+            <input type="password" className="form-control input-lg" placeholder="Password"/><br/>
+            <button type="button" className="btn btn-success btn-lg" >Login</button>
+            </div>
+            <div className="modal-footer">
+
+            </div>
+          </div>
+
+        </div>
+      </div>
       </div>
     )
   }
